67acc2e0fbaf171c8c98a091ced6c57a13b3936c
wesleydesouza <wesley_duarte122@hotmail.com>
2024-08-29 19:30:16 -0300

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
-export function App() {
-  return <></>;
-}
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Address } from "./pages/Address";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/painel",
+    element: <Home />,
+  },
+  {
+    path: "/address",
+    element: <Address />,
+  },
+]);
+
+export { router };
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { router } from './App'
+import { RouterProvider } from 'react-router-dom'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
